8ae04fa29b26ca5f0ef2cada79a346f6c43694b9
Angi Dávid <93470770+Neo7411@users.noreply.github.com>
2023-10-15 13:32:19 +0200

    win

--- Week07_lab6/3.py
+++ Week07_lab6/3.py
@@ -1,28 +1,26 @@
 from sense_hat import SenseHat
 import time
 
-# 3.1 Import necessary libraries
+
 sense = SenseHat()
 
-# 3.2 Create an object from the SenseHat class
-# 3.3 Create a variable (p) which keeps track of the rightmost point of the light beam
+
 p = [2, 3]
 light_len = 3
 space_size = 8
-speed = 1/7  # 3.3 Speed of the moving light
+speed = 1/7 
 
-# 3.4 Define the initial position of the light beam on the LED matrix
+
 r = (255, 0, 0)
 n = (0, 0, 0)
 
-# Initial light position
 space = [n] * space_size * space_size
 for i in range(light_len):
     space[p[1] * space_size + p[0] - i] = r
 
 sense.set_pixels(space)
 
-# 3.5 Create 2 functions which shift the light beam to the left side and to the right side
+
 def shift_right():
     global p
     p[0] = (p[0] + 1) % space_size
@@ -45,7 +43,7 @@ def shift_left():
             space[index] = r
     sense.set_pixels(space)
 
-# 3.6 Main function which is moving the light beam from right to left and left to right continuously
+
 def main():
     while True:
         while p[0] < space_size - 1:
@@ -56,5 +54,5 @@ def main():
             shift_left()
             time.sleep(speed)
 
-# Run the main function
+
 main()
--- Week07_lab6/4.py
+++ Week07_lab6/4.py
@@ -2,36 +2,34 @@ import time
 import random
 from sense_hat import SenseHat
 
-# Create an instance of the SenseHat class
+
 sense = SenseHat()
 
-# Define the blue color
+
 blue = (0, 0, 255)
 black = (0, 0, 0)
 
-# Initialize the LED matrix with all LEDs turned off
+
 display = [black] * 64
 
-# Function to create rain effect
-def rain_effect():
-    while True:
-        # Randomly turn on a LED in the first row with blue color
-        index = random.randint(0, 7)
-        display[index] = blue
+
+
+while True:
         
-        # Update the LED matrix display
-        sense.set_pixels(display)
+    index = random.randint(0, 7)
+    display[index] = blue
+        
+     
+    sense.set_pixels(display)
 
-        # Wait for 500ms
-        time.sleep(0.5)
+  
+    time.sleep(0.5)
 
-        # Shift all rows down
-        for i in range(55, -1, -1):
-            display[i+8] = display[i]
+    
+    for i in range(55, -1, -1):
+        display[i+8] = display[i]
 
-        # Turn off the LEDs in the first row
-        for i in range(8):
-            display[i] = black
+    for i in range(8):
+        display[i] = black
 
-# Create rain effect
-rain_effect()
+
