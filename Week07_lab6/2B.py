0d25b38d755ed4c4b65b678248fa926684581faf
Angi Dávid <93470770+Neo7411@users.noreply.github.com>
2023-10-31 10:46:26 +0100

    lab7 pacek

--- Week07_lab6/2B.py
+++ Week07_lab6/2B.py
@@ -1,59 +1,78 @@
 from sense_hat import SenseHat
 import time
 
+
 sense = SenseHat()
 
 state = 0
+old_state = 0
 
-w = (255,255,255)
-r = (255,0,0)
-g = (0,255,0)
-y = (255,255,0)
-n = (0,0,0)
+w = (255, 255, 255)
+r = (255, 0, 0)
+g = (0, 255, 0)
+y = (255, 255, 0)
+n = (0, 0, 0)
 
 red = [
- n, n, n, r, r, n, n, n,
- n, n, n, r, r, n, n, n,
- n, n, n, n, n, n, n, n,
- n, n, n, n, n, n, n, n,
- n, n, n, n, n, n, n, n,
- n, n, n, n, n, n, n, n,
- n, n, n, n, n, n, n, n,
- n, n, n, n, n, n, n, n
- ]
+    n, n, n, r, r, n, n, n,
+    n, n, n, r, r, n, n, n,
+    n, n, n, n, n, n, n, n,
+    n, n, n, n, n, n, n, n,
+    n, n, n, n, n, n, n, n,
+    n, n, n, n, n, n, n, n,
+    n, n, n, n, n, n, n, n,
+    n, n, n, n, n, n, n, n
+]
 
 red_yellow = [
- n, n, n, r, r, n, n, n,
- n, n, n, r, r, n, n, n,
- n, n, n, n, n, n, n, n,
- n, n, n, y, y, n, n, n,
- n, n, n, y, y, n, n, n,
- n, n, n, n, n, n, n, n,
- n, n, n, n, n, n, n, n,
- n, n, n, n, n, n, n, n
- ]
+    n, n, n, r, r, n, n, n,
+    n, n, n, r, r, n, n, n,
+    n, n, n, n, n, n, n, n,
+    n, n, n, y, y, n, n, n,
+    n, n, n, y, y, n, n, n,
+    n, n, n, n, n, n, n, n,
+    n, n, n, n, n, n, n, n,
+    n, n, n, n, n, n, n, n
+]
 
 yellow = [
- n, n, n, n, n, n, n, n,
- n, n, n, n, n, n, n, n,
- n, n, n, n, n, n, n, n,
- n, n, n, y, y, n, n, n,
- n, n, n, y, y, n, n, n,
- n, n, n, n, n, n, n, n,
- n, n, n, n, n, n, n, n,
- n, n, n, n, n, n, n, n
- ]
+    n, n, n, n, n, n, n, n,
+    n, n, n, n, n, n, n, n,
+    n, n, n, n, n, n, n, n,
+    n, n, n, y, y, n, n, n,
+    n, n, n, y, y, n, n, n,
+    n, n, n, n, n, n, n, n,
+    n, n, n, n, n, n, n, n,
+    n, n, n, n, n, n, n, n
+]
 
 green = [
- n, n, n, n, n, n, n, n,
- n, n, n, n, n, n, n, n,
- n, n, n, n, n, n, n, n,
- n, n, n, n, n, n, n, n,
- n, n, n, n, n, n, n, n,
- n, n, n, n, n, n, n, n,
- n, n, n, g, g, n, n, n,
- n, n, n, g, g, n, n, n
- ]
+    n, n, n, n, n, n, n, n,
+    n, n, n, n, n, n, n, n,
+    n, n, n, n, n, n, n, n,
+    n, n, n, n, n, n, n, n,
+    n, n, n, n, n, n, n, n,
+    n, n, n, n, n, n, n, n,
+    n, n, n, g, g, n, n, n,
+    n, n, n, g, g, n, n, n
+]
+
+
+def normal_state(color, duration):
+    sense.clear()
+    sense.set_pixels(color)
+    time.sleep(duration)
+
+
+def out_of_order_state():
+    sense.set_pixels(yellow)
+    time.sleep(0.5)
+    sense.clear()
+    time.sleep(0.5)
+
+
+def halt_state():
+    pass
 
 
 def set_state():
@@ -65,58 +84,49 @@ def set_state():
         state = 0
     else:
         pass
-def button_event(event):
+
+
+def out_of_order_event(event):
     global state
     if event.action == 'released':
-        if state != 4: 
+        if state != 4 and state != 5:
             state = 4
-        else:   
+        else:
             state = 3
 
 
-def sync_event(event):
+def halt_event(event):
     global state
-    act_state=state
-    if event.action == 'pressed':
-       while True:
-         time.sleep(1)
-         if sense.stick.direction_up.action=='released':
-            state=act_state
-            break
-    
-  
+    global old_state
+
+    if event.action == 'released':
+        if state != 5 and state != 4:
+            old_state = state
+            state = 5
+        else:
+            state = old_state
 
 
-
-def state_control(state):
-    if state == 0:
-        sense.set_pixels(red)
-        time.sleep(3)
-        sense.clear()
-    elif state == 1:
-        sense.set_pixels(red_yellow)
-        time.sleep(1)
-        sense.clear()
-    elif state == 2:
-        sense.set_pixels(green)
-        time.sleep(2)
-        sense.clear()
-    elif state == 3:
-        sense.set_pixels(yellow)
-        time.sleep(1)
-        sense.clear()
-    else:
-        sense.set_pixels(yellow)
-        time.sleep(0.5)
-        sense.clear()
-        time.sleep(0.5)
+sense.stick.direction_middle = out_of_order_event
+sense.stick.direction_up = halt_event
 
 
 def main():
     global state
-    sense.stick.direction_middle = button_event
-    sense.stick.direction_up = sync_event
     while True:
-        state_control(state)
+        if state == 0:
+            normal_state(red, 3)
+        elif state == 1:
+            normal_state(red_yellow, 1)
+        elif state == 2:
+            normal_state(green, 2)
+        elif state == 3:
+            normal_state(yellow, 1)
+        elif state == 4:
+            out_of_order_state()
+        else:
+            halt_state()
         set_state()
+
+
 main()
